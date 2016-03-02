aabdaead975d926872b9404cbcaaf9dd9925bc1f
Cristovao Verstraeten <cristovao@apleasantview.com>
2016-03-02 14:50:45 +0100

    Commit script

---
+++ ie-twtr-gh.py
@@ -0,0 +1,31 @@
+import time
+import tweepy
+
+# Keys, tokens and secrets
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
+
+# Tweepy OAuthHandler
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+# Variables
+target = input("Enter Twitter handle without @: ")
+user = api.get_user(target)
+
+print("User name: ", user.name)
+print("User followers count: ", user.followers_count)
+
+def limit_handled(cursor):
+    while True:
+        try:
+            yield cursor.next()
+        except tweepy.RateLimitError:
+            time.sleep(15 * 60)
+
+for follower in limit_handled(tweepy.Cursor(api.followers, target).items()):
+    if follower.followers_count > 1000:
+        print(follower.name + " | " + follower.screen_name + " | " , follower.followers_count)

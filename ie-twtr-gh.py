87d79803fff172337fb0edd57e1effe99098ed76
Cristovao Verstraeten <cristovao@apleasantview.com>
2016-03-04 14:42:21 +0100

    Variables more human readable. Alter exception rule. Small clean up

--- ie-twtr-gh.py
+++ ie-twtr-gh.py
@@ -1,9 +1,7 @@
 import sys
 import time
 from twitter import *
-# import win_unicode_console
 
-# win_unicode_console.enable()
 
 # Keys, tokens and secrets
 consumer_key = ""
@@ -15,23 +13,26 @@ access_token_secret = ""
 t = Twitter(auth = OAuth(access_token, access_token_secret, consumer_key, consumer_secret))
 
 target_name = sys.argv[1]
-target_count = 200
-target_cursor = -1
+count = 200
+cursor = -1
 
 target_info = t.users.show(screen_name=target_name)
-target_sn = target_info["name"]
-target_fc = target_info["followers_count"]
-target = t.followers.list(screen_name = target_name, count = target_count, target = target_cursor)
+real_name = target_info["name"]
+followers_count = target_info["followers_count"]
+print(real_name, file = sys.stderr)
+print(followers_count, file = sys.stderr)
 
-while target_cursor != 0:
+while cursor != 0:
     try:
-        print(target_sn)
-        print(target_fc)
+        target = t.followers.list(screen_name = target_name, count = count, cursor = cursor)
         followers = target["users"]
+        # print(cursor, file = sys.stderr)
+        # print(len(followers), file = sys.stderr)
+
         for follower in followers:
             print("{}, {}".format(follower["screen_name"], follower["followers_count"]))
-        target_cursor = target["next_cursor"]
-        print(target_cursor)        
+
+        cursor = target["next_cursor"]
     except TwitterHTTPError as e:
-        print("Rate limit exceeded!")
-        sys.exit(1)
+        print("Rate limit exceeded!", file = sys.stderr)
+        time.sleep(60)

96719ddaac36e6e3007e600f489f4526e6b5dc5b
Cristovao Verstraeten <cristovao@apleasantview.com>
2016-03-15 14:18:31 +0100

    Add test for command line check

--- test/test_ie_twtr_gh.py
+++ test/test_ie_twtr_gh.py
@@ -1,9 +1,25 @@
+import sys
 import unittest
+from unittest.mock import patch
+
+import ie_twtr_gh
+from twitter import *
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class MainTestCase(unittest.TestCase):
+    """ Main definition tests """
+
+    def test_target_check(self):
+        """ Checks for target as command line argument """
+        del sys.argv[1]
+        test_command = sys.argv[1]
+        function_command = ie_twtr_gh.target_check()
+        if test_command == "-v":
+            print("No arguments passed, moving on!")
+        elif (len(sys.argv[1]) > 0) & (test_command != "-v"):
+            self.assertEqual(function_command, test_command)
+
+    
 
 
 if __name__ == '__main__':
